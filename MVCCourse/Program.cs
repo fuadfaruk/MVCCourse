6ce1f8f4a69b79efe26d9ed00245a7a425be589d
Fuad123yuriygie <farukfuadhassan@gmail.com>
2026-03-13 00:22:08 +0600

    Added a Simple Login Syetem

--- MVCCourse/Program.cs
+++ MVCCourse/Program.cs
@@ -6,13 +6,59 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", (HttpContext context) => {
-    string html = @"<html><body>
-<h1>Hello, World!</h1>
-<br/>
-Welcome to this new world!
-</body>
-</html>";
-    WriteHtml(context, html);
+    WriteHtml(context, @$"
+        <!DOCTYPE html>
+        <html>
+        <body>
+        <h1>Login</h1>
+        <form method=""post"" action=""/login"">
+            <label for=""username"">Username:</label>
+            <input type=""text"" id=""username"" name=""username""><br><br>
+            <label for=""password"">Password:</label>
+            <input type=""password"" id=""password"" name=""password""><br><br>
+            <input type=""submit"" value=""Login"">
+        </form>
+        </body>
+        </html>
+    ");
+});
+
+app.MapPost("/login", (HttpContext context) => {
+    var form = context.Request.Form;
+    var username = form["username"];
+    var password = form["password"];
+    if (username == "admin" && password == "admin")
+    {
+        WriteHtml(context, @$"
+            <!docktype html>
+            <html>
+                <head><title>miniHTML</title></head>
+                <body>
+                    <h1>Simple Framework</h1>
+                    <br/>
+                    Welcome to our simple framework!
+                </body>
+                </html>
+        ");
+    }
+    else
+    {
+        WriteHtml(context, @$"
+            <!DOCTYPE html>
+            <html>
+            <body>
+            <h1>Login</h1>
+            <form method=""post"" action=""/login"">
+                <label for=""username"">Username:</label>
+                <input type=""text"" id=""username"" name=""username""><br><br>
+                <label for=""password"">Password:</label>
+                <input type=""password"" id=""password"" name=""password""><br><br>
+                <input type=""submit"" value=""Login"">
+            </form>
+            </body>
+            </html>
+        ");
+    }
 });
 
 app.Run();
