d3d7fd832d9b92faefb21ea187a9b688d782ae5d
fuadfaruk <fuadfaruk707@gmail.com>
2026-03-14 11:40:31 +0600

    Used Action Method to Handle a Request

---
+++ MVCCourse/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCourse.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            //return View();
+            return "Hello World!!!!!!!!!!!";
+        }
+        
+        public string About()
+        {
+            return "This is the about page.";
+        }
+    }
+}
--- MVCCourse/Program.cs
+++ MVCCourse/Program.cs
@@ -3,69 +3,14 @@ using System.Net.Mime;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/", (HttpContext context) => {
-    WriteHtml(context, @$"
-        <!DOCTYPE html>
-        <html>
-        <body>
-        <h1>Login</h1>
-        <form method=""post"" action=""/login"">
-            <label for=""username"">Username:</label>
-            <input type=""text"" id=""username"" name=""username""><br><br>
-            <label for=""password"">Password:</label>
-            <input type=""password"" id=""password"" name=""password""><br><br>
-            <input type=""submit"" value=""Login"">
-        </form>
-        </body>
-        </html>
-    ");
-});
+app.UseRouting();
 
-app.MapPost("/login", (HttpContext context) => {
-    var form = context.Request.Form;
-    var username = form["username"];
-    var password = form["password"];
-    if (username == "admin" && password == "admin")
-    {
-        WriteHtml(context, @$"
-            <!docktype html>
-            <html>
-                <head><title>miniHTML</title></head>
-                <body>
-                    <h1>Simple Framework</h1>
-                    <br/>
-                    Welcome to our simple framework!
-                </body>
-                </html>
-        ");
-    }
-    else
-    {
-        WriteHtml(context, @$"
-            <!DOCTYPE html>
-            <html>
-            <body>
-            <h1>Login</h1>
-            <form method=""post"" action=""/login"">
-                <label for=""username"">Username:</label>
-                <input type=""text"" id=""username"" name=""username""><br><br>
-                <label for=""password"">Password:</label>
-                <input type=""password"" id=""password"" name=""password""><br><br>
-                <input type=""submit"" value=""Login"">
-            </form>
-            </body>
-            </html>
-        ");
-    }
-});
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
-
-void WriteHtml(HttpContext context, string html)
-{
-    context.Response.ContentType = MediaTypeNames.Text.Html;
-    context.Response.ContentLength = Encoding.UTF8.GetByteCount(html);
-    context.Response.WriteAsync(html);
-}
