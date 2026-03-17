9025619960ea5996b737dd423dbaec7e520bf286
fuadfaruk <fuadfaruk707@gmail.com>
2026-03-17 13:43:41 +0600

    Added Basic Bootstrap and Navigation

--- MVCCourse/Program.cs
+++ MVCCourse/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
+app.UseStaticFiles();
+
 app.UseRouting();
 
 app.MapControllerRoute(
