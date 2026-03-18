6cb690f1f7622bb800a59884467d9c260d289417
fuadfaruk <fuadfaruk707@gmail.com>
2026-03-18 15:08:11 +0600

    Added Display Item in a Table with Basic HTML Edit Link

--- MVCCourse/Controllers/CategoriesController.cs
+++ MVCCourse/Controllers/CategoriesController.cs
@@ -8,7 +8,8 @@ namespace MVCCourse.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var categories = CategoryRepository.GetCategories();
+            return View(categories);
         }
 
         public IActionResult Edit(int? id)
